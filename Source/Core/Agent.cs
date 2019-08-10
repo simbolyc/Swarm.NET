b0e4e2b1035985824ae1ad9470748a4d6a9a3a95
Baljit Singh <basingh89@gmail.com>
2019-08-10 14:32:34 -0400

    Add Agent

---
+++ Source/Core/Agent.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Simbolyc.Swarm
+{
+    /// <summary>
+    /// An agent is the base of class of entities that participate in the swarm.
+    /// An agent is simply an entity that is universally uniquely identifiable.
+    /// Agent instances are immutable.
+    /// </summary>
+    public class Agent
+    {
+        /// <summary>
+        /// Universally uniquely identifier of the agent.
+        /// </summary>
+        public Guid ID { get; }
+
+        /// <summary>
+        /// Creates an instance of an agent with the given identifier.
+        /// </summary>
+        /// <param name="id">Universally uniquely identifier.</param>
+        public Agent(Guid id) => ID = id;
+
+        /// <summary>
+        /// Creates an instance of an agent with a random identifier.
+        /// </summary>
+        public Agent() => ID = Guid.NewGuid();
+
+        /// <summary>
+        /// Returns a string that represents the agent.
+        /// </summary>
+        /// <returns>A string that represents the agent.</returns>
+        public override string ToString() => GetType().Name + "{id = " + ID + "}";
+    }
+}
---
+++ Test/Core/AgentTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Xunit;
+
+namespace Simbolyc.Swarm
+{
+    /// <summary>
+    /// Unit tests for <see cref="Agent"/>.
+    /// </summary>
+    public sealed class AgentTest
+    {
+        [Fact]
+        public void DefaultConstructor()
+        {
+            Agent first = new Agent();
+            Agent second = new Agent();
+
+            // Check not empty
+            Assert.NotEqual(Guid.Empty, first.ID);
+            Assert.NotEqual(Guid.Empty, second.ID);
+
+            // Check random
+            Assert.NotEqual(first.ID, second.ID);
+        }
+
+        [Fact]
+        public void Constructor()
+        {
+            Guid id = Guid.NewGuid();
+            Assert.Equal(id, new Agent(id).ID);
+        }
+
+        [Fact]
+        public void StringConversion()
+        {
+            Agent agent = new Agent();
+            string agentStr = agent.ToString();
+
+            Assert.Contains(agent.GetType().Name, agentStr);
+            Assert.Contains(agent.ID.ToString(), agentStr);
+        }
+    }
+}
